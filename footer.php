d4f71842f28a8d5a1ff43fab964a91d70b8cd92f
Faresben19 <96697174+Faresben19@users.noreply.github.com>
2023-06-04 23:20:50 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="bootstrap.css"/>
+</head>
+
+<div class="pasdepage">
+    <div class="container-fluid">
+        <br>
+        <div class="row">
+            <br>
+            <div class="col-sm-4">
+                <h3>Contact</h3>
+                 <a href="connexionAdmin.php">&#x1F510;</a><br>
+               Benzidoun Fares<br>
+                Couvert Raphaël<br>
+
+                De Oliveira neige Alexandre<br>
+               Mallick Roman<br>
+            </div>
+            <div class="col-sm-4">
+                <h3>Réseaux</h3>
+                <a href="https://www.facebook.fr">Facebook</a><br>
+                <a href="https://www.twitter.fr">Twitter</a><br>
+                <a href="https://www.instagram.fr">Instagram</a><br>
+                <a href="https://www.linkedin.fr">LinkedIn</a><br>
+            </div>
+            <div class="col-sm-4">
+                <h3>Newsletter</h3>
+                <br>
+                <form action="inscriptionNewsletter.php" method="post">
+                    <div class="form-group">
+                        <input type="email" style="width: 400px;" name="Email"
+                               placeholder="Entrez votre adresse email pour recevoir la newsletter">
+                        <br><br>
+                        <button type="submit" class="btn btn-light btn-outline-primary">Inscrivez-vous</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <br>
+</div>
+</html>
