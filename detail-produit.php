ecc2f9b56f21e4e46deb716f4e8cc7c8e4d4604e
ALEXSNOW16 <105298784+ALEXSNOW16@users.noreply.github.com>
2023-06-04 23:35:49 +0200

    Create detail-produit.php

---
+++ detail-produit.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+      <link rel="stylesheet" href="styleIndex.css"/>
+  <title>Détails du produit</title>
+</head>
+
+<style>
+    
+    #content
+    {
+        height: 80%;
+        display:flex;
+    }
+    #img
+    {
+        height:70%;
+        border:solid;
+    }
+    #P1
